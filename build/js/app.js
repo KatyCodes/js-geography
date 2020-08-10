d46d13378bc4bfc020091b6b587914d76f1e8644
Katy Stucolov <katyannhenning@gmail.com>
2020-08-10 16:05:58 -0700

    removes all pins that have been set up by the last game

--- build/js/app.js
+++ build/js/app.js
@@ -7,11 +7,11 @@ var apiKey = require('./../.env').apiKey;
 
 function Map() {
   this.countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Botswana","Brazil","Bulgaria", "Cameroon","Chad","Chile","China","Colombia","Congo","Costa Rica","Croatia","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Italy","Jamaica","Japan","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Russia","Rwanda","Samoa","San Marino","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","Sudan","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Tunisia","Turkey","Turkmenistan","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Yemen","Zambia","Zimbabwe"];
-  this.score = 0;
+  //this.score = 0;
   this.userGuess = "";
   this.answer = "";
 }
-
+var score = 0;
 
 Map.prototype.randomCountry = function() {
   var length = this.countries.length;
@@ -20,13 +20,9 @@ Map.prototype.randomCountry = function() {
 };
 
 Map.prototype.playGame = function() {
+  var markerPosition;
   var _this = this;
   if (navigator.geolocation) {
-    // navigator.geolocation.getCurrentPosition(function(position) {
-      //var pos = {
-        //lat: -34.397,
-        //lng: position.coords.longitude
-      //};
       _this.map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 15.326572, lng: -25.326572},
         zoom: 2,
@@ -63,32 +59,36 @@ Map.prototype.playGame = function() {
           },
         ]
       });
-
+      if(markerPosition) {
+        markerPosition.setMap(null); };
       _this.map.setOptions({ draggableCursor: 'url("/../img/cursor.png"), auto' });
 
+      //see if the answer was correct by checking longitude and latitude
       _this.map.addListener("click", function (event) {
         var latitude = event.latLng.lat();
         var longitude = event.latLng.lng();
-        $.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&key=' + apiKey).then(function(response)
-        {
-          console.log(response);
+        $.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&key=' + apiKey).then(function(response) {
           response.results["0"].address_components.forEach(function(item) {
-
             if (item.types.includes('country')) {
               _this.userGuess = item.long_name;
             }
-          });
+            });
+
+            //if the answer is correct:
           if (_this.answer.toLowerCase() === _this.userGuess.toLowerCase()) {
             _this.score ++;
             $('#score').text(_this.score);
+            $('#country').html(_this.answer);
             $('#instruction').text("Good job! You are a real genius! Now go find:");
             _this.randomCountry();
-            $('#country').html(_this.answer);
+
+
           } else {
-            $('#instruction').text("Sorry, game over! That's " + _this.userGuess + ". See the map for the correct answer. Play again?");
+            //if the answer is incorrect
+            $('#instruction').text("Sorry, game over! You clicked on " + _this.userGuess + ". See the map for the location of the correct answer. Want to play again?");
             $('#next').show();
+            //add reset game button that clears the board and score
             $('#country').hide();
-            var markerPosition;
             $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + _this.answer + '&key=' + apiKey).then(function(response){
               markerPosition = response.results["0"].geometry.location;
               _this.marker = new google.maps.Marker({
